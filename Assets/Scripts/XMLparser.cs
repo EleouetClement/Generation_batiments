00a57aa676c5126ec6522987adab49bfc2f227b7
EleouetClement <eleouet.clement@outlook.fr>
2021-12-06 13:21:44 +0100

    Parser Ko

--- Assets/Scripts/XMLparser.cs
+++ Assets/Scripts/XMLparser.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Xml.Linq;
+using System.Linq;
 using System.Xml;
+using System.Xml.Linq;
+
+//690290B1027
 
 public class XMLparser : MonoBehaviour
 {
@@ -10,7 +14,7 @@ public class XMLparser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadData();
     }
 
     // Update is called once per frame
@@ -23,9 +27,27 @@ public class XMLparser : MonoBehaviour
     public void LoadData()
     {
         XDocument doc = XDocument.Load(filePath);
-        XmlReader reader = doc.CreateReader();
+        XElement indoorFeatures = doc.Root;
 
+        XNamespace XsGml = indoorFeatures.GetNamespaceOfPrefix("gml");
+        XNamespace XBldg = indoorFeatures.GetNamespaceOfPrefix("bldg");
+        XNamespace Xapp = indoorFeatures.GetNamespaceOfPrefix("app");
+        XNamespace Xcore = indoorFeatures.GetNamespaceOfPrefix("core");
 
+        var results = doc.Elements(Xcore + "cityObjectMember").Select(
+            x=> new
+            {
+                boundaries = x.Descendants(XBldg + "Building").Select(
+                    y=> new
+                    {
+                        date = y.Descendants(Xcore + "creationDate")
+                    })
+            }).FirstOrDefault();
+
+        foreach(var item in results.boundaries)
+        {
+            Debug.Log(item.date);
+        }
     }
 
 
